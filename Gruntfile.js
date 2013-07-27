e6464b515c79c206aab192ae8c8df23bd0045b75
faceach <faceach@gmail.com>
2013-07-28 00:19:23 +0800

    regroup file organization

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,99 +1,125 @@
 module.exports = function(grunt) {
-  'use strict';
 
-  var EMPTY = "empty:";
-
-  //
-  // Grunt configuration:
-  //
-  // https://github.com/cowboy/grunt/blob/master/docs/getting_started.md
-  //
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
-    config: {
-      src: 'src/',
-      dist: 'dist/',
-      temp: 'temp/'
-    },
-    clean: {
-      build: ["<%= config.dist %>"]
-    },
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          flatten: true,
-          src: [""],
-          dest: '<%= config.dist %>',
-          filter: 'isFile'
-        }]
-      }
-    },
-    less: {
-      development: {
-        options: {
-          paths: []
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/**\n' +
+                '* <%= pkg.name %>.js v<%= pkg.version %> by @fat and @mdo\n' +
+                '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                '* <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' +
+                '*/\n',
+        jqueryCheck: 'if (!jQuery) { throw new Error(\"Bootstrap requires jQuery\") }\n\n',
+        // Task configuration.
+        clean: {
+            dist: ['dist']
         },
-        files: {
-        }
-      },
-      production: {
-        options: {
-          paths: [],
-          compress: true
+        concat: {
+            options: {
+                banner: '<%= banner %><%= jqueryCheck %>',
+                stripBanners: false
+            },
+            bootstrap: {
+                src: ['js/transition.js', 'js/alert.js', 'js/button.js', 'js/carousel.js', 'js/collapse.js', 'js/dropdown.js', 'js/modal.js', 'js/tooltip.js', 'js/popover.js', 'js/scrollspy.js', 'js/tab.js', 'js/affix.js'],
+                dest: 'dist/js/<%= pkg.name %>.js'
+            }
         },
-        files: {
-        }
-      }
-    },
-    uglify: {
-      options: {
-        mangle: true,
-        sourceMap: '<%= config.dist %>scripts/source-map.js'
-      },
-      dist: {
-        files: {
-          '<%= config.dist %>scripts/page/boot.min.js': ['<%= config.dist %>scripts/page/boot.js']
-        }
-      }
-    },
-    imagemin: {
-      dist: {
-        options: {
-          optimizationLevel: 3
+        jshint: {
+            options: {
+                jshintrc: 'js/.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            src: {
+                src: ['js/*.js']
+            },
+            test: {
+                src: ['js/tests/unit/*.js']
+            }
         },
-        files: {
+        recess: {
+            options: {
+                compile: true
+            },
+            bootstrap: {
+                files: {
+                    'dist/css/bootstrap.css': ['less/bootstrap.less']
+                }
+            },
+            min: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    'dist/css/bootstrap.min.css': ['less/bootstrap.less']
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            bootstrap: {
+                files: {
+                    'dist/js/<%= pkg.name %>.min.js': ['<%= concat.bootstrap.dest %>']
+                }
+            }
+        },
+        qunit: {
+            options: {
+                inject: 'js/tests/unit/phantom.js'
+            },
+            files: ['js/tests/*.html']
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 3000,
+                    base: '.'
+                }
+            }
+        },
+        watch: {
+            src: {
+                files: '<%= jshint.src.src %>',
+                tasks: ['jshint:src', 'qunit']
+            },
+            test: {
+                files: '<%= jshint.test.src %>',
+                tasks: ['jshint:test', 'qunit']
+            },
+            recess: {
+                files: 'less/*.less',
+                tasks: ['recess']
+            }
         }
-      }
-    }
-  });
+    });
 
-  //grunt.loadNpmTasks('grunt-contrib');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-string-replace');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  // Default task.
-  grunt.registerTask('default', [
-    'clean:build',
-    'copy:dist',
-    'less:development',
-    'less:production',
-    'requirejs',
-    'string-replace:html',
-    'string-replace:css',
-    'string-replace:js',
-    'uglify',
-    'imagemin'
-  ]);
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-recess');
 
-  // Alias the `test` task to run the `mocha` task instead
-  //grunt.registerTask('test', 'server:phantom mocha');
 
+    // Test task.
+    grunt.registerTask('test', ['jshint', 'qunit']);
+
+    // JS distribution task.
+    grunt.registerTask('dist-js', ['concat', 'uglify']);
+
+    // CSS distribution task.
+    grunt.registerTask('dist-css', ['recess']);
+
+    // Full distribution task.
+    grunt.registerTask('dist', ['clean', 'dist-css', 'dist-js']);
+
+    // Default task.
+    grunt.registerTask('default', ['test', 'dist']);
 };
